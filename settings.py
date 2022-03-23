94f039923d050d96af0b6eda92f73064a2d9a924
Alex <azmerana03@gmail.com>
2022-03-23 11:14:01 -0400

    Added tab tracking to settings

--- settings.py
+++ settings.py
@@ -1,5 +1,12 @@
+from re import S
 from tokenizer import Tokenizer
 
 def init(inputString = ""):
     global t
     t = Tokenizer(inputString)
+    global s
+    s = 0
+
+def printTabs():
+    for i in range(s):
+        print("\t", end='')
